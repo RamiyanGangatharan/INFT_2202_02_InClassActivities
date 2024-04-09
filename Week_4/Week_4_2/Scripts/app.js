754955de2882d6b27fb5a8cac94e12e332559151
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-04-08 20:50:10 -0400

    Week 4 Commit

---
+++ Week_4/Week_4_2/Scripts/app.js
@@ -0,0 +1,194 @@
+"use strict";
+
+// IIFE == immediately invoked functional expression
+// AKA == Anonymous Self-Executing Function
+(
+    function()
+    {
+        function AddContact(fullname, ContactNumber, EmailAddress)
+        {
+            let contact = new core.Contact(fullname, ContactNumber, EmailAddress);
+
+            if(contact.serialize())
+            {
+                let key = contact.fullName.substring(0,1) * Date.now();
+                localStorage.setItem(key, contact.serialize());
+            }
+        }
+        function displayHomepage()
+        {
+            console.log("Called Displayed Homepage.");
+
+            $("#AboutUsBTN").on("click", () => {location.href = "../about.html";})
+            $("main").append(`<p id="MainParagraph" class="mt-3">This is my first paragraph</p>`)
+            $('body').append(`<article class="container"><p id="ArticleParagraph" class = "mt-3"> This is my article paragraph</p></article>`)
+        }
+
+        function displayProductsPage()
+        {
+            console.log("Called Displayed Products Page.");
+        }
+
+        function displayContactUsPage()
+        {
+            console.log("Called Displayed Contact Us Page.");
+
+            let sendButton = document.getElementById("sendButton");
+            let subscribeButton = document.getElementById("subscribeButton");
+
+            sendButton.addEventListener("click", function()
+            {
+                if(subscribeButton.checked)
+                {
+                    let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                    if(contact.serialize())
+                    {
+                        let key = contact.fullName.substring(0, 1) + Date.now();
+                        localStorage.setItem(key, contact.serialize())
+                    }
+                }
+            })
+        }
+
+        function displayContactListPage()
+        {
+            console.log("Called Displayed Contact List Page.");
+
+            if(localStorage.length > 0)
+            {
+                let contactList = document.getElementById("contactList")
+                let data = "";
+                let keys = Object.keys(localStorage);
+                let index = 1;
+
+                for(const key of keys)
+                {
+                    let contact = new Contact();
+                    let contactData = localStorage.getItem(key);
+                    contact.deserialize(contactData);
+
+                    data +=
+                        `
+                             <tr>
+                                <td>
+                                    ${contact.fullName}
+                                </td>
+                                <td>
+                                    ${contact.contactNumber}
+                                </td>
+                                <td>
+                                    ${contact.emailAddress}
+                                </td>
+                                <td class="text-center">
+                                    <button value="${key}" class="btn-primary btn-sm edit">Edit</button>
+                                </td>
+                                <td class="text-center">
+                                    <button value="${key}" class="btn btn-danger btn-sm delete">Delete</button>
+                                </td>
+                            </tr>
+                        `
+                    index++;
+                }
+                contactList.innerHTML = data;
+            }
+
+            $("#addButton").on("Click", () =>
+            {
+                location.href = "../edit.ejs#add";
+            })
+
+            $("button.delete").on("click", () =>
+            {
+                if(confirm("Please confirm contact deletion?"))
+                {
+                    localStorage.removeItem($(this).val())
+                }
+                location.href = "../contact-list.html";
+            })
+
+            $("button.edit").on("click", () =>
+            {
+                location.href = "../edit.ejs#edit" + $(this).val();
+            })
+        }
+
+        function displayEditPage()
+        {
+            console.log("Edit page called")
+            let page = location.hash.substring(1);
+
+            switch (page)
+            {
+                case "add":
+
+                    $("main>h1").text("Add Contact");
+                    $("#editButton").html("<i class=\"fas fa-plu-circle fa-sm\"></i>")
+                    $("#editButton").on("click", (event) =>
+                    {
+                        event.preventDefault();
+                        AddContact(fullName.value(), contactNumber.value(), emailAddress.value() )
+                        location.href = "../contact-list.html";
+                    })
+                    break;
+                default:
+
+                    // edit contact
+                    let contact = new  core.Contact();
+                    contact.deserialize(localStorage.getItem(page));
+
+                    $("#fullName").val(contact.fullName);
+                    $("#contactNumber").val(contact.contactNumber);
+                    $("#email").val(contact.emailAddress);
+
+                    $("#editButton").on("click", (event) =>
+                    {
+                        event.preventDefault();
+                        contact.fullName = $("#fullName").val();
+                        contact.contactNumber = $("#contactNumber").val();
+                        contact.emailAddress = $("#email").val();
+
+                        // replace the contact in local storage
+                        localStorage.setItem(page, contact.serialize());
+                        location.href = "../contact-list.html";
+                    })
+
+                    $("#cancelButton").on("click", () =>
+                    {
+                        location.href = "../contact-list.html";
+                    })
+                    break;
+            }
+        }
+
+        function displayServicesPage()
+        {
+            console.log("Called Displayed Our Services Page.");
+        }
+
+        function Start()
+        {
+            console.log("App Started.");
+            switch (document.title)
+            {
+                case "Home":
+                    displayHomepage();
+                    break;
+                case "Our Products":
+                    displayProductsPage();
+                    break;
+                case "Our Services":
+                    displayServicesPage();
+                    break;
+                case "Contact Us":
+                    displayContactUsPage();
+                    break;
+
+                case "Contact List":
+                    displayContactListPage();
+                    break;
+            }
+        }
+        window.addEventListener("load", Start);
+    }
+    ()
+)
